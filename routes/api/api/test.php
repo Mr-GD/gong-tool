a2a0d4f3391cac296ffec38985bef05deae96926
龚德铭 <>
2025-01-03 15:01:42 +0800

    gdm/项目搭建

--- routes/api/api/test.php
+++ routes/api/api/test.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('test/index', [TestController::class, 'index']);
 Route::post('test/checkRequest', [TestController::class, 'checkRequest']);
+
+
+
+
+//Route::group(['prefix' => 'user'], function () {
+//    Route::get('/profile', function () {
+//        return 'User Profile';
+//    });
+//    Route::get('/posts', function () {
+//        return 'User Posts';
+//    });
+//});
+//再次分组后，最终的接口请求路由是：
+///api/user/profile
+///api/user/posts
