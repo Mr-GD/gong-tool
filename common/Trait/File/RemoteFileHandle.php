e659ae1e13c6e4ea622c6b43982ba395505215fe
龚德铭 <gongdeming@yidian51.com>
2025-03-14 15:26:04 +0800

    refactor(RemoteFileHandle): 优化 formatStorageFileUrl 方法- 将方法参数 $addressField 添加，默认值为 'url' - 使用 $this->{$addressField}替代直接使用 $this->url - 优化了对不同存储模式下文件 URL 的处理逻辑

--- common/Trait/File/RemoteFileHandle.php
+++ common/Trait/File/RemoteFileHandle.php
@@ -13,19 +13,19 @@ trait RemoteFileHandle
      * @author 龚德铭
      * @date 2025/1/2 20:00
      */
-    function formatStorageFileUrl()
+    function formatStorageFileUrl($addressField = 'url')
     {
-        if (str_contains($this->url, 'http:') || str_contains($this->url, 'https:')) {
-            return $this->url;
+        if (str_contains($this->{$addressField}, 'http:') || str_contains($this->url, 'https:')) {
+            return $this->{$addressField};
         }
 
         $return = '';
         switch ($this->storage_mode) {
             case Mode::LOCAL:
-                $return = $this->url;
+                $return = $this->{$addressField};
                 break;
             case Mode::KODBOX:
-                $url    = rtrim($this->url, '/');
+                $url    = rtrim($this->{$addressField}, '/');
                 $return = env('UPLOAD_FILE_KODBOX_API_URL') . $url;
                 $return = $return ? $return . '&accessToken=' . KodboxUpload::instance()->getAccessToken() : '';
                 break;
