b043513304736c6f0dfec3d4575e2ecea7e8779a
龚德铭 <>
2025-03-24 20:19:09 +0800

    refactor(common): 优化代码异常处理和文档注释
    - 在 Alapi.php 和 PracticalToolKit.php 中添加 Exception 引用
    - 更新函数注释中的异常类型，从 \Exception 改为 Exception- 优化代码结构，提高可读性和维护性

--- common/Tool/ExternalRequest/Alapi.php
+++ common/Tool/ExternalRequest/Alapi.php
@@ -5,6 +5,7 @@ namespace common\Tool\ExternalRequest;
 use App\Exceptions\ErrException;
 use common\Exception\Code;
 use common\Tool\Base\Request\InterfaceRequest;
+use Exception;
 
 /**
  * alapi请求
@@ -43,7 +44,7 @@ class Alapi extends InterfaceRequest
      * 身份证信息查询
      * @param string $idCard
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      * @date 2025/3/24 11:31
      * https://www.alapi.cn/api/35/api_document
      * {"request_id":"726938435625402369","success":true,"message":"success","code":200,"data":{"address_code":"441282","abandoned":1,"address":"广东省肇庆市罗定市","province":"广东省","city":"肇庆市","county":"罗定市","birthday":"1981-01-01","constellation":"摩羯座","zodiac":"酉鸡","sex":1,"length":18,"age":43},"time":1734275628,"usage":0}
--- common/Tool/ExternalRequest/PracticalToolKit.php
+++ common/Tool/ExternalRequest/PracticalToolKit.php
@@ -5,6 +5,7 @@ namespace common\Tool\ExternalRequest;
 use App\Exceptions\ErrException;
 use common\Exception\Code;
 use common\Tool\Base\Request\InterfaceRequest;
+use Exception;
 use Illuminate\Support\Str;
 
 /**
@@ -61,7 +62,7 @@ class PracticalToolKit extends InterfaceRequest
      * IP查询
      * @param string $ip
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      * @date 2025/3/24 16:30
      * https://www.idcd.com/docs/open-api/ip
      * response:{"status": true,"code": 200,"message": "Success","request_id": "467f3111-e469-4490-b3ca-5819be2c236d","data": {"ip": "125.71.160.214","country": "中国","region": "四川","city": "成都市","county": "温江区","isp": "电信","area": "中国四川成都市 电信/(温江)电信","long": "","lat": ""}}
@@ -79,7 +80,7 @@ class PracticalToolKit extends InterfaceRequest
      * 历史上的今天
      * @param string $date
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      * @date 2025/3/24 16:40
      * https://www.idcd.com/docs/open-api/today-in-history
      * response:{"status": true,"code": 200,"message": "Success","request_id": "1a8091da-380c-4f3e-a933-73da4e29161d","data": [{"year": 280,"month": 5,"day": 1,"data": "西晋将领王濬兵临建业，吴帝孙皓出降，立国52年，历五帝的孙吴宣告灭亡，三国时代正式结束。","date": "280年5月1日"}]}
@@ -100,7 +101,7 @@ class PracticalToolKit extends InterfaceRequest
      * 人物肖像抠图去背景
      * @param string $url
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      * @date 2025/3/24 17:28
      * https://www.idcd.com/docs/open-api/image-remove-bg
      * response:{"status": true,"code": 200,"message": "OK","request_id": "f70d6168-d494-4aa7-a443-e57f314b9c4e","data": {"url": "https://open.19981.com/files/remove_bg/output_download_image_20250216195826.png"}}
