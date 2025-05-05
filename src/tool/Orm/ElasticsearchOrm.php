588d265900b9ad3a79cf0f2de0baf23e62b25e9f
龚德铭 <gongdeming@yidian51.com>
2025-05-05 14:13:26 +0800

    update

--- src/tool/Orm/ElasticsearchOrm.php
+++ src/tool/Orm/ElasticsearchOrm.php
@@ -6,6 +6,11 @@ use gong\tool\base\api\Orm\ElasticsearchConfigure;
 use Vae\PhpElasticsearchOrm\Builder;
 use Vae\PhpElasticsearchOrm\Factory;
 
+/**
+ * 需要引入 vae/php-elasticsearch-orm 包
+ * composer install vae/php-elasticsearch-orm
+ * 但由于该包仅支持php 7.4 且与laravel 11冲突，故此移除这个功能
+ */
 class ElasticsearchOrm extends Factory
 {
     public Builder $builder;
