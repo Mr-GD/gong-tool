94c54f53cae624837d642c050a3784860fae52b4
龚德铭 <1026709547@qq.com>
2025-05-16 11:31:49 +0800

    refactor(helper): 添加 instance 方法的文档注释
    - 在 Instance.php 文件中的 instance 方法上添加了文档注释
    - 注释说明了该方法的作用，并指出了返回类型为 static

--- src/helper/traits/Instance.php
+++ src/helper/traits/Instance.php
@@ -4,7 +4,10 @@ namespace gong\helper\traits;
 
 trait Instance
 {
-    /** 实例 */
+    /**
+     * 实例
+     * @return static
+     */
     public static function instance()
     {
         return new static(...func_get_args());
