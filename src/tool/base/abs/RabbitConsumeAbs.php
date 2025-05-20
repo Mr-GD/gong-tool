f272dd17af37b95ca9ae076f61037b6a8a5d51f8
龚德铭 <1026709547@qq.com>
2025-05-20 13:41:51 +0800

    refactor(tool): 移除 RabbitConsumeAbs 类中的未使用方法
    - 删除了 RabbitConsumeAbs 类中的 fail 方法
    - 该方法为空方法，没有实际功能

--- src/tool/base/abs/RabbitConsumeAbs.php
+++ src/tool/base/abs/RabbitConsumeAbs.php
@@ -16,9 +16,4 @@ abstract class RabbitConsumeAbs implements RabbitMqConsume
         $this->params = $params;
         $this->assignParameter($this->params);
     }
-
-    public function fail(\gong\tool\Rabbitmq\RabbitMq $mq, \Throwable $e)
-    {
-
-    }
 }
