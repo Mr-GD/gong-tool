c2b44d8025c1a968d6389b29b425ce866ee87eaf
龚德铭 <1026709547@qq.com>
2025-09-01 11:01:29 +0800

    refactor(tool): 重构 Excel导出功能
    - 调整类属性访问权限，提高封装性
    - 优化工作表切换和自定义表头功能
    - 改进渲染字段设置方式- 修复文件下载时的文件名编码问题
    - 移除未使用的扩展检查方法
    - 优化代码结构，提高可读性和可维护性

--- src/tool/base/abs/Excel/XlswriterExport.php
+++ src/tool/base/abs/Excel/XlswriterExport.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpExpressionResultUnusedInspection */
 
 namespace gong\tool\base\abs\Excel;
 
@@ -10,6 +10,8 @@ use Vtiful\Kernel\Excel;
  * Excel文件导出基类
  * @method $this setWhetherPage(bool $whetherPage) 设置是否分页导出
  * @method $this setDownload(bool $isDownload) 是否下载文件
+ *
+ * https://xlswriter-docs.viest.me/zh-cn
  */
 abstract class XlswriterExport
 {
@@ -24,14 +26,18 @@ abstract class XlswriterExport
     /**
      * @var Excel
      */
-    public $excel;
+    protected $excel;
 
     /**
      * @var array
      */
-    public $savePath;
-    public $lines = 1;
+    protected $savePath;
+    protected $lines = 1;
     protected $download = true;
+
+    protected $path = '';
+
+    protected $sheetName = 'Sheet1';
 
     /**
      * @var bool 是否为分页导出
@@ -51,27 +57,81 @@ abstract class XlswriterExport
 
     public function __construct(string $savePath = '')
     {
-        $this->initialise($savePath);
+        $this->path = $savePath;
+    }
+
+    /**
+     * 自定义表头
+     * @return Excel
+     */
+    protected function customHeader()
+    {
+        return $this->excel->header(array_values($this->title));
+    }
+
+    /**
+     * 工作表
+     */
+    protected function sheetNames(): array
+    {
+        return [
+            'Sheet1'
+        ];
+    }
+
+    /**
+     * 设置从第几行开始写数据
+     * @return int
+     */
+    protected function setLiens()
+    {
+        return 1;
     }
 
     public function export()
     {
+        $this->beforeExport();
+        $this->initialise();
         ob_start();
-        /** 分页导出 */
-        if ($this->whetherPage) {
-            $this->queryDataBatches();
-        } else {
-            $this->exportData = $this->setExportData();
-            $this->render();
+
+        foreach ($this->sheetNames() as $sheetName) {
+            if ($this->sheetName != $sheetName) {
+                $this->excel->addSheet($sheetName);
+                $this->sheetName = $sheetName;
+            }
+            $this->customHeader();
+            $this->lines = $this->setLiens();
+            /** 分页导出 */
+            if ($this->whetherPage) {
+                $this->queryDataBatches();
+            } else {
+                $this->exportData = $this->setExportData();
+                $this->render();
+            }
         }
+
+        /** 没效果，应该是这个版本不支持，但官方文档上可以这样调用 */
+//        if ($this->sheetName != 'Sheet1') {
+//            /** 切换到Sheet1工作表并隐藏它 */
+//            $this->excel->checkoutSheet('Sheet1')->setCurrentSheetHide();
+//        }
 
         return $this->execution();
     }
 
     /**
+     * 设置渲染字段
+     * @return array
+     */
+    protected function setRenderFields(): array
+    {
+        return array_keys($this->title);
+    }
+
+    /**
      * 分页查询
      */
-    public function queryDataBatches()
+    protected function queryDataBatches()
     {
         while (true) {
             $this->exportData = $this->setExportData();
@@ -83,11 +143,10 @@ abstract class XlswriterExport
         $this->exportData = [];
     }
 
-    public function render()
+    protected function render()
     {
-        $this->beforeExport();
         $methodExists = method_exists($this, 'customProcessing');
-        $fields       = array_keys($this->title);
+        $fields       = $this->setRenderFields();
         foreach (renderYield($this->exportData) as $data) {
             $column = 0;
             foreach ($fields as $field) {
@@ -99,27 +158,17 @@ abstract class XlswriterExport
         }
     }
 
-    public function initialise($savePath)
+    protected function initialise()
     {
         set_time_limit(0);
         ini_set('memory_limit', -1);
-        $this->extension();
-        $this->savePath = $this->setDir($savePath);
+        $this->savePath = $this->setDir();
         $this->title    = $this->setTitle();
         $this->fileName = $this->setFileName();
         $excel          = new Excel($this->config($this->savePath));
-        $this->excel    = $excel->fileName($this->fileName . '.xlsx')
-                                ->header(array_values($this->title))
-        ;
+        $this->excel    = $excel->fileName($this->fileName . '.xlsx');
         if ($this->whetherPage) {
             $this->excel->constMemory($this->fileName . '.xlsx');
-        }
-    }
-
-    public function extension()
-    {
-        if (!extension_loaded('xlswriter')) {
-            throw new \Exception('请安装xlswriter扩展！');
         }
     }
 
@@ -127,15 +176,15 @@ abstract class XlswriterExport
      * 执行
      * @return string|void
      */
-    public function execution()
+    protected function execution()
     {
+        $filePath = $this->excel->output();
         /** 不是下载 */
         if (!$this->download) {
-            return $this->excel->output(); // 返回文件地址 本地文件地址 全路径 例：/www/wwwroot/flash_sign_cloud/console/runtime/SaveLocalFile/xxx.xlsx
+            return $filePath; // 返回文件地址 本地文件地址 全路径 例：/www/wwwroot/flash_sign_cloud/console/runtime/SaveLocalFile/xxx.xlsx
         }
 
         $this->setHeader();
-        $filePath = $this->excel->output();
 
         // 注册关闭函数，在脚本结束时删除文件
         register_shutdown_function(function () use ($filePath) {
@@ -150,7 +199,7 @@ abstract class XlswriterExport
         exit;
     }
 
-    public function config($savePath)
+    protected function config($savePath)
     {
         if (!is_dir($savePath)) {
             @mkdir($savePath, 0755, true);
@@ -166,11 +215,11 @@ abstract class XlswriterExport
         return $return;
     }
 
-    public function setDir(string $savePath = '')
+    protected function setDir()
     {
         $dir = variable()->get('runtime_dir', '/');
-        if ($savePath) {
-            $dir .= $savePath;
+        if ($this->path) {
+            $dir .= $this->path;
         } else {
             $classDir = explode('\\', get_called_class());
             $classDir = array_splice($classDir, -2, 2);
@@ -180,10 +229,10 @@ abstract class XlswriterExport
         return $dir;
     }
 
-    public function setHeader()
+    protected function setHeader()
     {
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header(sprintf('Content-Disposition: attachment;filename="%s.xlsx"', $this->fileName));
+        header(sprintf('Content-Disposition: attachment;filename="%s.xlsx"', urlencode($this->fileName)));
         header('Cache-Control: max-age=0');
         header('Pragma: public');
     }
@@ -217,5 +266,23 @@ abstract class XlswriterExport
         }
 
         return '';
+    }
+
+    /**
+     * 将数字列号转换为Excel字母列名
+     * @param $column
+     * @return string 对应的字母列名
+     */
+    protected function numberToColumnLetter($column)
+    {
+        $letter = '';
+        while ($column > 0) {
+            // 计算当前位的字母
+            $remainder = ($column - 1) % 26;
+            $letter    = chr(65 + $remainder) . $letter;
+            // 处理下一位
+            $column = (int)(($column - $remainder) / 26);
+        }
+        return $letter;
     }
 }
