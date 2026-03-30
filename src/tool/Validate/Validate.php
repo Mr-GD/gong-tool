88f4b81904f90f4c79da45d334eb7b0a8dd573e2
龚德铭 <1026709547@qq.com>
2026-03-30 14:15:14 +0800

    1

---
+++ src/tool/Validate/Validate.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace gong\tool\Validate;
+
+/**
+ * 验证器
+ * 基于think 验证器规则
+ * https://doc.thinkphp.cn/v8_0/validator.html
+ */
+abstract class Validate extends \think\Validate
+{
+    /**
+     * @return array
+     * @doc 场景
+     */
+    abstract protected function scenarios(): array;
+
+    /**
+     * @return array
+     * @doc 字段翻译
+     */
+    abstract protected function translates(): array;
+
+    /**
+     * @return array
+     * @doc 规则
+     */
+    abstract protected function regulation(): array;
+
+    /**
+     * @param $data
+     * @param string $scene
+     * @doc 验证
+     * 实际使用时最好重写这个方法，便于使用框架的导演抛出
+     */
+    public static function validator($data, string $scene = '')
+    {
+        $make          = validate(static::class);
+        $make->rule    = $make->regulation();
+        $make->message = $make->translates();
+        if ($scene) {
+            $make->scene($scene);
+        }
+        $make->check($data);
+    }
+}
